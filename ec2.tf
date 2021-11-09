5b0f2b66883fe5d0e691c16aa692f342f799ee17
Ubuntu <ubuntu@ip-172-31-19-136.ec2.internal>
2021-11-09 08:26:53 +0000

    ec2 added

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "Amol-web-node-" {
+  ami = "ami-083654bd07b5da81d"
+#  name = "UbuntuTerraform"
+  instance_type = "t2.micro"
+  key_name = "Terraform"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+provider "aws"{
+  region="us-east-1"
+ # access_key = "**************8"
+ # secret_key = "************************"
+}
